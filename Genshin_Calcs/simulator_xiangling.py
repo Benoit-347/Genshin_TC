70a8ec05b1db16df5378243dd636f738361217bf
Benoit K <benoitk.347@gmail.com>
2024-11-23 12:54:20 +0530

    Update simulator_xiangling.py
    Added em, er rolls in sim, included buffs fn

--- Genshin_Calcs/simulator_xiangling.py
+++ Genshin_Calcs/simulator_xiangling.py
@@ -1,23 +1,11 @@
 import damage_calculator as calc
 def calc_xiangling(atk, cr, cd, em, er_):
     temp_dmg_ = dmg_ + er_/4
-    return calc.calc_real_damage(calc.calc_raw_damage(atk, temp_dmg_, cr, cd, skill, 1.5, em), 90, 90, 0.4, 0, 0)
-atk = 0
-er_ = 1
-em = 0
-ele_dmg_ = 0
-burst_dmg_ = 0
-dmg_ = ele_dmg_ + burst_dmg_
-cr = 0
-cd = 0
-res_shred = 0
-def_shred = 0
-skill = 0
-
+    return calc.calc_real_damage(calc.calc_raw_damage(atk, temp_dmg_, cr, cd, skill, 1.5, em), 90, 90, 0, 0, 0)
 def get_intristic_stat():
     char_base_atk = 225.14
     ascention_ = 96
-    passive_buff = 0    #pyro res shred = 0.15
+    passive_buff = 0.15    #pyro res shred = 0.15
     skill_ = 2.016*8
     weapon_base_atk = 	510
     weapon_secondary = 0
@@ -26,6 +14,20 @@ def get_intristic_stat():
     return char_base_atk+weapon_base_atk, ascention_, passive_buff, skill_, weapon_secondary, weapon_passive_1, weapon_passive_2
 
 base_atk, ascention, passive_buff, skill, weapon_secondary, dmg_, cr = get_intristic_stat()
+
+atk_ = 0
+atk = calc.calc_atk(base_atk, atk_)
+er_ = 1 + weapon_secondary
+em = 0 + ascention
+ele_dmg_ = 0
+burst_dmg_ = 0
+dmg_ = dmg_ + ele_dmg_ + burst_dmg_
+cr = cr + 0.05
+cd = 0.5
+res_shred = 0 + passive_buff
+def_shred = 0
+skill = skill
+
 
 def update_values_from_artifact_set():
     global er_, ele_dmg_, cr
@@ -38,6 +40,21 @@ def update_values_from_artifact_set():
     cr += 0.311
     #dmg_ += er/4
 
+def update_buffs(bennet= 0, kazuha= 0, xilonen= 0, candace= 0):
+    if bennet == 1:
+        atk += 600
+        atk_+= 0.2
+    if kazuha == 1:
+        res_shred += 0.4
+        dmg_ += 0.58
+    if xilonen == 1:
+        res_shred += 0.36
+        dmg_ += 0.4
+        atk_ += 0.48
+    if canadace == 1:
+        dmg_ += 0.3
+        em += 120
+        
 def get_count(list):
     set1 = set(list)
     counted = []
@@ -81,7 +98,6 @@ def get_optimal(total_rolls):
 
     #copying stats
     sim_atk = atk
-    sim_atk = calc.calc_atk(base_atk, 0)
     sim_em = em
     sim_er = er_
     sim_dmg_ = dmg_
@@ -117,20 +133,30 @@ def get_optimal(total_rolls):
             temp = cd_dmg
         if atk_dmg > temp:
             current_best = "atk"
+        if em_dmg > temp:
+            current_best = "em"
+            temp = em_dmg
+        if er_dmg > temp:
+            current_best = "er"
+            temp = er_dmg
             
         if current_best == "cr":
             sim_cr += cr_roll
         elif current_best == "cd":
             sim_cd += cd_roll
-        else:
+        elif current_best == "em":
+            sim_em += em_roll
+        elif current_best == "atk":
             sim_atk += atk_roll
+        else:
+            sim_er += er_roll
         sim_list.append(current_best)
     print(get_count(sim_list))
             
 
 
 #main
-cr += 0.05
-cd += 0.5
+
 update_values_from_artifact_set()
+update_buffs(bennet= 1, kazuha= 1)
 get_optimal(31)
